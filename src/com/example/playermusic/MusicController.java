3fa253404e1f5ee6b4e597efc80c0990ed071736
dummyprogrammer <milearzvn@gmail.com>
2015-08-17 21:59:25 +0300

    Initial commit.

---
+++ src/com/example/playermusic/MusicController.java
@@ -0,0 +1,20 @@
+package com.example.playermusic;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController 
+{
+	
+	public MusicController(Context c)
+	{
+		super(c);
+	}
+	 
+	public void hide()
+	{
+		
+	}
+	 
+}
---
+++ src/com/example/playermusic/SongAdapter.java
@@ -0,0 +1,94 @@
+package com.example.playermusic;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import java.util.ArrayList;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class SongAdapter extends BaseAdapter 
+{
+	 
+	private ArrayList<Song> songs;
+	private LayoutInflater songInf;
+	
+	private LinearLayout songLay;
+	
+	private TextView artistView;
+	private TextView songView;
+	private Song currSong;
+	
+	
+	public SongAdapter(Context c, ArrayList<Song> theSongs)
+	{
+		songs=theSongs;
+		songInf=LayoutInflater.from(c);
+	}
+	
+	
+	@Override
+	public int getCount() 
+	{
+		return songs.size();
+	}
+	 
+	@Override
+	public Object getItem(int arg0) 
+	{
+	    // TODO Auto-generated method stub
+	    return null;
+	}
+	 
+	@Override
+	public long getItemId(int arg0) 
+	{
+	    // TODO Auto-generated method stub
+	    return 0;
+	}
+	 
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) 
+	{
+		//map to song layout
+		songLay = (LinearLayout)songInf.inflate
+	      (R.layout.song, parent, false);
+		
+		//get title and artist views
+		songView = (TextView)songLay.findViewById(R.id.song_title);
+		artistView = (TextView)songLay.findViewById(R.id.song_artist);
+		
+		//get song using position
+		currSong = songs.get(position);
+		
+		//get title and artist strings
+		songView.setText(currSong.getTitle());
+		artistView.setText(currSong.getArtist());
+		
+		/*
+		songLay.setOnClickListener(new OnClickListener() 
+		{
+            public void onClick(View v) 
+            {
+            	//do stuff here
+            	artistView.setBackgroundColor(Color.CYAN);
+            }
+        });
+		*/
+		
+		//set position as tag
+		songLay.setTag(position);
+		return songLay;
+	}
+	
+	
+
+ 
+}
