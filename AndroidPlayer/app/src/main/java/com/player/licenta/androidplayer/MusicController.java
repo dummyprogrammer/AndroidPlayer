96ad27f3a0646cb6e36e9623f1441e62018676ed
dummyprogrammer <milearzvn@gmail.com>
2015-08-17 22:48:34 +0300

    Re-created the AndroidStudio project.

---
+++ AndroidPlayer/app/src/main/java/com/player/licenta/androidplayer/MusicController.java
@@ -0,0 +1,20 @@
+package com.player.licenta.androidplayer;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController 
+{
+	
+	public MusicController(Context c)
+	{
+		super(c);
+	}
+	 
+	public void hide()
+	{
+		
+	}
+	 
+}
